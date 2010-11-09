eed14cfd3b53fa890e8e3c973b8c8445ec2adf16
Loïc Frering <loic.frering@gmail.com>
2010-11-10 00:32:14 +0100

    Add forgotten resource loading for DAO in sandbox.

--- sandbox/application/modules/scaffold/Bootstrap.php
+++ sandbox/application/modules/scaffold/Bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 class Scaffold_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-
+    protected function _initResourceLoader()
+    {
+        $resourceLoader = $this->getResourceLoader();
+        $resourceLoader->addResourceType('dao', 'dao', 'Dao');
+    }
 }
